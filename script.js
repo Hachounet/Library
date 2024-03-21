bb2c82d80c554b68e00d8b6c8c563022d4a3d6a5
Hachounet <hachounet.github@gmx.fr>
2024-03-21 13:32:57 +0100

    Validation form feature.

--- script.js
+++ script.js
@@ -1,118 +1,145 @@
 const myLibrary = [];
 
-function Book (title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        console.log(this.title, this.author, this.pages, this.read)
-    }
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    console.log(this.title, this.author, this.pages, this.read);
+  };
 }
-
 
 function addBooktoLibrary() {
+  let titleInput = document.getElementById('title');
+  let authorInput = document.getElementById('author');
+  let pagesInput = document.getElementById('pages');
+  let readInput = document.getElementById('read');
 
-    let titleInput = document.getElementById("title");
-    let authorInput = document.getElementById("author");
-    let pagesInput = document.getElementById("pages");
-    let readInput = document.getElementById("read");
+  let title = titleInput.value;
+  let author = authorInput.value;
+  let pages = pagesInput.value;
+  let read = readInput.checked;
 
-    let title = titleInput.value;
-    let author = authorInput.value;
-    let pages = pagesInput.value;
-    let read = readInput.checked;
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
 
-    const newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
+  displayBooks();
 
-    displayBooks();
-
-    titleInput.value = "";
-    authorInput.value = "";
-    pagesInput.value = "";
-    readInput.checked = false;
+  titleInput.value = '';
+  authorInput.value = '';
+  pagesInput.value = '';
+  readInput.checked = false;
 }
 
-
 function displayBooks() {
-    document.getElementById("cardzone").innerHTML = "";
+  document.getElementById('cardzone').innerHTML = '';
 
-    myLibrary.forEach((book, index) => {
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("card");
-        newDiv.dataset.number = index;
+  myLibrary.forEach((book, index) => {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('card');
+    newDiv.dataset.number = index;
 
-        const bookTitle = document.createElement("p");
-        bookTitle.textContent = book.title;
-        bookTitle.classList.add("data", "bookTitle");
-        newDiv.appendChild(bookTitle);
+    const bookTitle = document.createElement('p');
+    bookTitle.textContent = book.title;
+    bookTitle.classList.add('data', 'bookTitle');
+    newDiv.appendChild(bookTitle);
 
-        const bookAuthor = document.createElement("p");
-        bookAuthor.textContent = book.author;
-        bookAuthor.classList.add("data", "bookAuthor");
-        newDiv.appendChild(bookAuthor);
+    const bookAuthor = document.createElement('p');
+    bookAuthor.textContent = book.author;
+    bookAuthor.classList.add('data', 'bookAuthor');
+    newDiv.appendChild(bookAuthor);
 
-        const bookPages = document.createElement("p");
-        bookPages.textContent = book.pages;
-        bookPages.classList.add("data", "bookPages");
-        newDiv.appendChild(bookPages);
+    const bookPages = document.createElement('p');
+    bookPages.textContent = book.pages;
+    bookPages.classList.add('data', 'bookPages');
+    newDiv.appendChild(bookPages);
 
+    const bookRead = document.createElement('div');
+    bookRead.className = 'bookRead';
+    bookRead.classList.add('data');
+    newDiv.appendChild(bookRead);
 
-        const bookRead = document.createElement("div");
-        bookRead.className = "bookRead";
-        bookRead.classList.add("data")
-        newDiv.appendChild(bookRead);
+    const bookReadLabel = document.createElement('label');
+    bookReadLabel.classList.add('toggle');
+    bookRead.appendChild(bookReadLabel);
 
-        const bookReadLabel = document.createElement("label");
-        bookReadLabel.classList.add("toggle");
-        bookRead.appendChild(bookReadLabel)
-
-        const bookReadInput = document.createElement("input");
-        bookReadInput.setAttribute("type", "checkbox");
-        bookReadInput.checked = book.read;
-        bookReadInput.id = "checkbox" + index;
-        bookReadInput.addEventListener("click", () => {
-            myLibrary[index].read = bookReadInput.checked;
-        });
-        bookReadLabel.appendChild(bookReadInput);
-
-        const bookReadSpan = document.createElement("span");
-        bookReadSpan.classList.add("slider");
-        bookReadLabel.appendChild(bookReadSpan);
-
-        const bookReadSpan2 = document.createElement("span");
-        bookReadSpan2.classList.add("labels");
-        bookReadSpan2.setAttribute("data-on", "READ")
-        bookReadSpan2.setAttribute("data-off", "UNREAD")
-        bookReadLabel.appendChild(bookReadSpan2);
-
-        const deleteBtn = document.createElement("button");
-        deleteBtn.textContent = "X";
-        deleteBtn.classList.add("deleteBtn");
-        deleteBtn.id = "deleteBtn"+ index;
-
-
-        deleteBtn.addEventListener("click", () => {
-            myLibrary.splice(index, 1);
-            displayBooks();
-        });
-        newDiv.appendChild(deleteBtn);
-
-        const selectDad = document.getElementById("cardzone");
-        selectDad.appendChild(newDiv);
+    const bookReadInput = document.createElement('input');
+    bookReadInput.setAttribute('type', 'checkbox');
+    bookReadInput.checked = book.read;
+    bookReadInput.id = 'checkbox' + index;
+    bookReadInput.addEventListener('click', () => {
+      myLibrary[index].read = bookReadInput.checked;
     });
+    bookReadLabel.appendChild(bookReadInput);
+
+    const bookReadSpan = document.createElement('span');
+    bookReadSpan.classList.add('slider');
+    bookReadLabel.appendChild(bookReadSpan);
+
+    const bookReadSpan2 = document.createElement('span');
+    bookReadSpan2.classList.add('labels');
+    bookReadSpan2.setAttribute('data-on', 'READ');
+    bookReadSpan2.setAttribute('data-off', 'UNREAD');
+    bookReadLabel.appendChild(bookReadSpan2);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'X';
+    deleteBtn.classList.add('deleteBtn');
+    deleteBtn.id = 'deleteBtn' + index;
+
+    deleteBtn.addEventListener('click', () => {
+      myLibrary.splice(index, 1);
+      displayBooks();
+    });
+    newDiv.appendChild(deleteBtn);
+
+    const selectDad = document.getElementById('cardzone');
+    selectDad.appendChild(newDiv);
+  });
 }
 
 function menuBook() {
-    let menu = document.getElementById("menu");
+  let menu = document.getElementById('menu');
 
-    menu.style.display = (menu.style.display === "none" || menu.style.display === "") ? "grid" : "none";
-
+  menu.style.display =
+    menu.style.display === 'none' || menu.style.display === ''
+      ? 'grid'
+      : 'none';
 }
 
+function checkDataForm(inputID) {
+  const inputToCheck = document.getElementById(inputID);
 
-document.getElementById("addbook").addEventListener("click", menuBook );
+  const validityInput = inputToCheck.validity;
 
-document.getElementById("closeBtn").addEventListener("click", menuBook);
-document.getElementById("addToBtn").addEventListener("click", addBooktoLibrary);
+  if (validityInput.rangeUnderflow) {
+    inputToCheck.setCustomValidity('Cannot be less than 5 pages.');
+  } else {
+    inputToCheck.setCustomValidity('');
+  }
+
+  return inputToCheck.reportValidity();
+}
+
+document.getElementById('addToBtn').addEventListener('click', () => {
+  const inputs = document.querySelectorAll('.form_input');
+  let allInputsValid = true;
+
+  inputs.forEach((input) => {
+    const inputID = String(input.id);
+    console.log(inputID);
+    if (!checkDataForm(inputID)) {
+      allInputsValid = false;
+      console.log(allInputsValid);
+    }
+  });
+  if (allInputsValid) {
+    console.log(allInputsValid);
+    addBooktoLibrary();
+  }
+});
+
+document.getElementById('addbook').addEventListener('click', menuBook);
+
+document.getElementById('closeBtn').addEventListener('click', menuBook);
